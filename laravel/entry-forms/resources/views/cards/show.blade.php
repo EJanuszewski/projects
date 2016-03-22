3c9ddc230fab49343455a649eb75f15c6b4c958c
Edward Januszewski <e.januszewski@gmail.com>
2016-03-22 21:20:15 +0000

    Storing data with forms

--- laravel/entry-forms/resources/views/cards/show.blade.php
+++ laravel/entry-forms/resources/views/cards/show.blade.php
@@ -2,11 +2,27 @@
 
 @section('content')
 
-    {{ $card->title }}
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <h1>{{ $card->title }}</h1>
 
-    <ul>
-        @foreach($card->notes as $note)
-            <li>{{ $note->body }}</li>
-        @endforeach
-    </ul>
+            <ul class="list-group">
+                @foreach($card->notes as $note)
+                    <li class="list-group-item">{{ $note->body }}</li>
+                @endforeach
+            </ul>
+
+            <h3>Add New Note</h3>
+
+            <form method="POST" action="/cards/{{ $card->id }}/notes">
+                <div class="form-group">
+                    <textarea name="body" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Add Note" class="btn btn-primary" />
+                </div>
+            </form>
+
+        </div>
+    </div>
 @endsection
--- laravel/entry-forms/resources/views/master.blade.php
+++ laravel/entry-forms/resources/views/master.blade.php
@@ -2,10 +2,13 @@
 <html>
 <head>
     <title>Laravel</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
     <link rel="stylesheet" href="{{ elixir('css/app.css') }}" />
 
 </head>
 <body>
-    @yield('content')
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
